7b336f7a23f83f26d04b7d8e4d1055100ce6e058
ilham <ilhamnopihendri@gmail.com>
2025-01-22 02:42:08 +0700

    refactor(Card): add address.street in card component.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,7 @@
 const Card = ({ monster }) => {
-    const { id, name, email } = monster
+    const { id, name, email, address } = monster
     return (
-        <div className=" flex flex-col justify-center text-center">
+        <div className=" flex flex-col justify-center text-center items-center">
             <figure className="w-full flex justify-center">
                 <img
                     src={`https://robohash.org/${id}?set=set=2&size=180x180`}
@@ -10,7 +10,8 @@ const Card = ({ monster }) => {
                 />
             </figure>
             <h2 className="mt-4 text-2xl text-white font-bold tracking-[0.1rem]">{name}</h2>
-            <p className="mt-2 tex">{email.toLowerCase()}</p>
+            <p className="mt-2 text-xl text-slate-900">{email.toLowerCase()}</p>
+            <p className="mt-2 text-xs text-slate-800  w-1/2 text-center">📍 {address.street}</p>
         </div>
     )
 }
