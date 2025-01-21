2a2376d7271ce49f895b6e33c74f8befefad0653
ilham <ilhamnopihendri@gmail.com>
2025-01-21 21:24:26 +0700

    refactor(App): Refactor App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+// Import Component Header, SearchBox, dan CardLIst
 import Header from './components/Header/index';
 import SearchBox from './components/SearchBox';
+import CardList from './components/CardList/index';
+
 function App() {
   const [searcField, setSearchField] = useState("")
-  const [title, setTitle] = useState("")
-  const [monster, setMonster] = useState([])
+  const [monsters, setMonsters] = useState([])
+  const [filteredMOnsters, setFilteredMonsters] = useState(monsters);
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  // Mengambil data dari API
+  useEffect(() => {
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then((users) => setMonsters(users))
+  }, [])
+
+
+  // Memfilter monsters berdasarkan input pencarian
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searcField)
+    })
+    setFilteredMonsters(newFilteredMonsters)
+  }, [monsters, searcField])
+
+
+  // Mengupdate State berdasarkan perubahan Input pencarian
+  const onSearchChange = (e) => {
+    const searchFieldString = e.target.value.toLowerCase();
+    setSearchField(searchFieldString)
+  }
 
   return (
     <div className=' min-h-screen h-full w-full '>
       <Header />
-      <SearchBox />
-    </div>
+      <SearchBox type='search' onChangeHandler={onSearchChange} Placeholder={"Search Monsters..."} />
+      <CardList monsters={filteredMOnsters} />
+    </div >
   )
 }
 
