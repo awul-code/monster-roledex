747a8f95b64666d8f0e9138e5f84b658679bb16b
ilham <ilhamnopihendri@gmail.com>
2025-01-21 21:22:32 +0700

    refactor(Card): Refactor card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,16 @@
 const Card = ({ monster }) => {
-    const { name, email, id } = monster
+    const { id, name, email } = monster
     return (
-        <div className="card-container">
-            <img src={`https://robohash.org/${id}?set=set=2&size=180x180`} alt={`${name}`} />
-            <h2>{name}</h2>
-            <p>{email}</p>
+        <div className=" flex flex-col justify-center text-center">
+            <figure className="w-full flex justify-center">
+                <img
+                    src={`https://robohash.org/${id}?set=set=2&size=180x180`}
+                    alt={`${name}`}
+                    className="object-contain w-full max-w-56"
+                />
+            </figure>
+            <h2 className="mt-4 text-2xl text-white font-bold tracking-[0.1rem]">{name}</h2>
+            <p className="mt-2 tex">{email.toLowerCase()}</p>
         </div>
     )
 }
