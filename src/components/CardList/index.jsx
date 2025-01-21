ff1cf3c371377691b33280fdc5df9c376c57cae4
ilham <ilhamnopihendri@gmail.com>
2025-01-21 21:23:57 +0700

    refactor(Card List): Refactor CardList Component

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -1,12 +1,11 @@
 import Card from '../Card/index';
 const CardList = ({ monsters }) => {
+
     return (
-        <div className="w-[85vw] my-0 mx-auto grid grid-cols-4 gap-5">
-            {
-                monsters.map((monster) => {
-                    <Card monster={monster} key={monster.id} />
-                })
-            }
+        <div className="w-[85vw] my-0 mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 pb-40">
+            {monsters.map((monster) => {
+                return <Card monster={monster} key={monster.id} />
+            })}
         </div>
     )
 }
